235d725abe53dd7469f65748e1ed74a974a06b88
Your Name <you@example.com>
2021-08-16 16:47:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function countdown() {
 
     const totalSeconds = (newYearsDate - currentDate) / 1000
 
-    const days = Math.floor(totalSeconds / 3600 / 24)
+    const days = totalSeconds / 3600 / 24
     const hours = Math.floor(totalSeconds / 3600) % 24
     const mins = Math.floor(totalSeconds / 60) % 60
     const secs = Math.floor(totalSeconds) % 60
